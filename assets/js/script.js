c9f2bf6890ebf8361882f622a4d48d687b133830
Vaibhav0820 <83344569+Vaibhav0820@users.noreply.github.com>
2021-04-28 16:21:10 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,80 @@
+$('document').ready(function () {
+    //OWL CAROUSEL - KNOWLEDGE
+    $('#movieList1').owlCarousel({
+        loop: false,
+        margin: 10,
+        nav: true,
+        navText: [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        dots: true,
+        autoplay: false,
+        autoWidth: true,
+    })
+   
+    $('#movieList2').owlCarousel({
+        loop: false,
+        margin: 10,
+        nav: true,
+        navText: [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        dots: true,
+        autoplay: false,
+        autoWidth: true,
+    })
+
+    $('#movieList3').owlCarousel({
+        loop: false,
+        margin: 10,
+        nav: true,
+        navText: [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        dots: true,
+        autoplay: false,
+        autoWidth: true,
+    })
+
+    $('#movieList4').owlCarousel({
+        loop: false,
+        margin: 10,
+        nav: true,
+        navText: [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        dots: true,
+        autoplay: false,
+        autoWidth: true,
+    })
+
+    $('#movieList5').owlCarousel({
+        loop: false,
+        margin: 10,
+        nav: true,
+        navText: [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        dots: true,
+        autoplay: false,
+        autoWidth: true,
+    })
+
+    $('#movieList6').owlCarousel({
+        loop: false,
+        margin: 10,
+        nav: true,
+        navText: [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        dots: true,
+        autoplay: false,
+        autoWidth: true,
+    })
+});
